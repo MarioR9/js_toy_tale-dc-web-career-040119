7d83bb4003282f78142d247233edaba774b765e5
MarioR9 <mariorodriguezan@gmail.com>
2019-05-16 23:49:09 -0400

    Post Method saves to API and renders to page

--- README.md
+++ README.md
@@ -83,3 +83,8 @@ body:
   "likes": <new number>
 }
 ```
+
+
+"name": "Jessie",
+    "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+    "likes": 0
--- src/index.js
+++ src/index.js
@@ -1,41 +1,78 @@
+
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
-addToyInfo()
 
+document.addEventListener('DOMContentLoaded', function (){
+  fetchAllToys()
+  postToy()
 
-// YOUR CODE HERE
-function addToyInfo(){
+})
+
+function fetchAllToys(){
+  fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(toysData => {toysData.forEach(addToyInfo)
+  })
+}
+
+function addToyInfo(toy){
     let toyCollection = document.querySelector('#toy-collection')
     let cardDiv = document.createElement('div')
         cardDiv.className = 'card'
       let nameH2 = document.createElement('h2')
       let imgTag = document.createElement('img')
         imgTag.className = 'toy-avatar'
-        imgTag.src = "toy_image_url"//not real URL 
+        // imgTag.src = "toy_image_url"//not real URL 
       let likesTag = document.createElement('p')
       let btnTag = document.createElement('button')
+          nameH2.innerText = toy.name 
+          imgTag.src = toy.image
+          likesTag.innerText = toy.likes
     toyCollection.append(cardDiv)
     cardDiv.append(nameH2,imgTag,likesTag,btnTag)
 }
 
-
-
-
-
-
-
-
 addBtn.addEventListener('click', () => {
+      
   // hide & seek with the form
   addToy = !addToy
   if (addToy) {
     toyForm.style.display = 'block'
     // submit listener here
+    
   } else {
     toyForm.style.display = 'none'
+    
   }
 })
 
 
+
 // OR HERE!
+function postToy(){
+  let btn = document.getElementById('sub')
+  btn.addEventListener('click', function(event){
+    event.preventDefault()
+  let name = document.getElementsByClassName('input-text')[0].value
+  let img = document.getElementsByClassName('input-text')[1].value
+  fetch('http://localhost:3000/toys',{
+    method:"post",
+    headers: 
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body:   JSON.stringify({
+    
+      name: name,
+      image: img,
+      likes: 0
+    })
+  })
+  .then(response => response.json())
+  .then(newToy => {addToyInfo(newToy)
+  })
+})
+}
+
