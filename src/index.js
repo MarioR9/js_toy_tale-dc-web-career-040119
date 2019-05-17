d6ccf885e24d9d4675402ed54521b6237f337ee9
Marior9 <mariorodriguezan@gmail.com>
2019-05-17 13:52:51 -0400

    Done Toy Creating and likes- likes are not goint to display if server is not updated first.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ let addToy = false
 document.addEventListener('DOMContentLoaded', function (){
   fetchAllToys()
   postToy()
+ 
 
 })
 
@@ -25,13 +26,28 @@ function addToyInfo(toy){
         imgTag.className = 'toy-avatar'
         // imgTag.src = "toy_image_url"//not real URL 
       let likesTag = document.createElement('p')
+          likesTag.id = 'likesTag'
       let btnTag = document.createElement('button')
+          likesTag.innerText = toy.likes
+          btnTag.id = 'likesBtn'
+          btnTag.innerText = '<3'
+          btnTag.dataset.objId = toy.id
+          btnTag.addEventListener('click', function(){
+            incLikes(btnTag,likesTag)
+          })
           nameH2.innerText = toy.name 
           imgTag.src = toy.image
-          likesTag.innerText = toy.likes
+          
+          
+          
+          
     toyCollection.append(cardDiv)
     cardDiv.append(nameH2,imgTag,likesTag,btnTag)
+    
+    
 }
+
+
 
 addBtn.addEventListener('click', () => {
       
@@ -47,10 +63,9 @@ addBtn.addEventListener('click', () => {
   }
 })
 
-
-
 // OR HERE!
 function postToy(){
+  
   let btn = document.getElementById('sub')
   btn.addEventListener('click', function(event){
     event.preventDefault()
@@ -75,4 +90,32 @@ function postToy(){
   })
 })
 }
+
+    
+function incLikes(btnTag,likesTag){
+   
+    let btnId = btnTag.dataset.objId //"number"
+    let newLike = parseInt(likesTag.innerText) + 1
+          
+    fetch(`http://localhost:3000/toys/${parseInt(btnId)}`,{ 
+        method: 'PATCH',//pathing to data base
+        headers: 
+        {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+        
+          "likes": newLike
+          
+        })
+      })
+        .then(response => response.json())
+        .then(newToy => {
+          likesTag.innerHTML = newLike
+        })
+    
+}
+
+
 
