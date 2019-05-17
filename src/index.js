c80835506c47e44275aaf98dc85d857f990dc396
Marior9 <mariorodriguezan@gmail.com>
2019-05-16 20:34:55 -0400

    step 2 and 3 done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
+addToyInfo()
+
 
 // YOUR CODE HERE
+function addToyInfo(){
+    let toyCollection = document.querySelector('#toy-collection')
+    let cardDiv = document.createElement('div')
+        cardDiv.className = 'card'
+      let nameH2 = document.createElement('h2')
+      let imgTag = document.createElement('img')
+        imgTag.className = 'toy-avatar'
+        imgTag.src = "toy_image_url"//not real URL 
+      let likesTag = document.createElement('p')
+      let btnTag = document.createElement('button')
+    toyCollection.append(cardDiv)
+    cardDiv.append(nameH2,imgTag,likesTag,btnTag)
+}
+
+
+
+
+
+
+
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
